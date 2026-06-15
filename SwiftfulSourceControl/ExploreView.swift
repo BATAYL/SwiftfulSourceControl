5b2b9bc14fa68643f5356f2501fd18d1fb55ece8
Brian <Bman8384@yahoo.com>
2026-06-15 07:01:03 -0500

    [Feature] Add ExploreView

---
+++ SwiftfulSourceControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Brian Taylor on 6/15/26.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
