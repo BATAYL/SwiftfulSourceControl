117f4ac4aa44f948c2c313a9644a70d97555d8a1
Brian <Bman8384@yahoo.com>
2026-06-15 05:50:18 -0500

    [Clean] Re-order commit message doc

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -9,19 +9,23 @@
  COMMIT MESSAGES
  
  New feature:
- [Feature] Description of the featuer
- 
- Bug in Production
- [PATCH] Description of the patch
+ [Feature] Description of the feature
  
  Bug not in Produciton:
  [Bug] Description of the bug
- 
- Mundane tasks:
- [Clean] Description of changes
+
+ Release:
+ [Release] Description of release
+
+  Bug in Production
+ [PATCH] Description of the patch
  
  Release:
  [Release] Description of release
+
+ Mundane tasks:
+ [Clean] Description of changes
+ 
  */
 
 
