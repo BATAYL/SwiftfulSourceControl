0e169faf13cea08f1178848cb465b305ef04febb
Brian <Bman8384@yahoo.com>
2026-06-16 05:05:10 -0500

    [Feat] fix globe

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     ForEach(0..<20) { _ in
-                        Image(systemName: "magnifyingglass")
+                        Image(systemName: "globe")
                             .font(.largeTitle)
                             .imageScale(.large)
                             .foregroundStyle(.tint)
