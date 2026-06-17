2349ee9e39b343072a6e55a7770c4e8abe942610
Brian <Bman8384@yahoo.com>
2026-06-17 05:05:21 -0500

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Brian Taylor on 6/17/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Brian")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
